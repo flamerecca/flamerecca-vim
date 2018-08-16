33e9798125e243423a2fbd1063e4cd82fd1e1022
recca <flamerecca711@gmail.com>
2018-08-17 01:15:24 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,24 @@
 
 app_name="flamerecca-vim"
 
+home_variable_check() {
+    if [ -z "$HOME" ]; then
+        error "You must have your HOME environmental variable set to continue."
+    fi
+}
+
+backup() {
+	backup_target=$HOME/.vimrc
+	# if backup_target exists
+	if [ -e "$backup_target" ]; then
+		backup_path=$HOME/.backup-vim
+		mkdir -p $backup_path
+		mv $HOME/.vimrc $backup_path
+		res="$?"
+		success "~/.vimrc already backup to ${backup_path}."
+	fi
+}
+
 ################# main()
+
+home_variable_check
